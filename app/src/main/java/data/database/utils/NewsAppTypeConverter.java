41f1431f38f8d7ff0ed03f73da40c76b9ee0060c
swanjala <wanjala.sam.s@gmail.com>
2019-03-23 00:47:01 +0300

    (add): news app type converter
    - Converts list to joint Strings

--- app/src/main/java/data/database/utils/NewsAppTypeConverter.java
+++ app/src/main/java/data/database/utils/NewsAppTypeConverter.java
@@ -1,4 +1,16 @@
 package data.database.utils;
 
+import android.annotation.SuppressLint;
+import android.arch.persistence.room.TypeConverter;
+import android.arch.persistence.room.util.StringUtil;
+
+import java.util.List;
+
+@SuppressLint("RestrictedApi")
 public class NewsAppTypeConverter {
+
+    @TypeConverter
+    public static String intListToString(List<Integer> ints) {
+        return StringUtil.joinIntoString(ints);
+    }
 }
