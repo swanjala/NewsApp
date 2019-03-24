38f380a1c243c52c10d556967f5c39dfcb15fb8f
swanjala <wanjala.sam.s@gmail.com>
2019-03-24 12:49:58 +0300

    (add): source pojo
    - Create a pojo for news sources

--- app/src/main/java/data/datamodels/Source.java
+++ app/src/main/java/data/datamodels/Source.java
@@ -15,6 +15,30 @@ public class Source {
     @Expose
     private String name;
 
+    @SerializedName("description")
+    @Expose
+    private String description;
+
+    @SerializedName("url")
+    @Expose
+    private String url;
+
+    @SerializedName("category")
+    @Expose
+    private String category;
+
+
+    @SerializedName("language")
+    @Expose
+    private String language;
+
+
+    @SerializedName("country")
+    @Expose
+    private String country;
+
+
+
     public Object getId() {
         return id;
     }
@@ -27,7 +51,49 @@ public class Source {
         return name;
     }
 
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getUrl() {
+        return url;
+    }
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
+
+
