48fe8ed609024c21bdf683ab7097f7d35837ef74
swanjala <wanjala.sam.s@gmail.com>
2019-03-26 18:57:23 +0300

    (add): type converter for database

--- app/src/main/java/data/database/utils/NewsAppTypeConverter.java
+++ app/src/main/java/data/database/utils/NewsAppTypeConverter.java
@@ -2,15 +2,28 @@ package data.database.utils;
 
 import android.annotation.SuppressLint;
 import android.arch.persistence.room.TypeConverter;
+import android.arch.persistence.room.TypeConverters;
 import android.arch.persistence.room.util.StringUtil;
 
-import java.util.List;
+import com.google.gson.Gson;
 
-@SuppressLint("RestrictedApi")
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import data.datamodels.Sources;
+
 public class NewsAppTypeConverter {
 
+    Gson gson = new Gson();
     @TypeConverter
-    public static String intListToString(List<Integer> ints) {
-        return StringUtil.joinIntoString(ints);
+    public String fromSource(Set source) {
+
+        String json = " ";
+        for (int i = 0; i < source.size() ; i++) {
+             json = json.concat( gson.toJson(source));
+
+        }
+        return json;
     }
 }
