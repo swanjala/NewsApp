35b61899e6b514ae425644c3483ec36f4aa659a1
swanjala <wanjala.sam.s@gmail.com>
2019-03-26 18:43:45 +0300

    (ft): add categories fragment

---
+++ app/src/main/java/com/example/newsapp/adapters/CountryAdapter.java
@@ -0,0 +1,4 @@
+package com.example.newsapp.adapters;
+
+public class CountryAdapter {
+}
--- app/src/main/java/data/database/utils/ArticleDatabase.java
+++
@@ -1,9 +0,0 @@
-package data.database.utils;
-
-import data.database.accessobjects.ArticleAccessObject;
-
-
-public abstract class ArticleDatabase  {
-
-    public abstract ArticleAccessObject articleAccessObject();
-}
