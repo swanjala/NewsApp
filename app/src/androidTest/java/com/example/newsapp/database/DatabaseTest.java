c506cc5f870811458657d482cc27738e1c9b3149
swanjala <wanjala.sam.s@gmail.com>
2019-04-03 01:23:20 +0300

    (ft): implement a database instance
    - Initializes room and closes it after use

--- app/src/androidTest/java/com/example/newsapp/database/DatabaseTest.java
+++ app/src/androidTest/java/com/example/newsapp/database/DatabaseTest.java
@@ -1,4 +1,27 @@
 package com.example.newsapp.database;
 
+import android.arch.persistence.room.Room;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
+
+import data.database.AppDatabase;
+
 public class DatabaseTest {
+
+    protected AppDatabase appDatabase;
+
+    @Before
+    public void init() {
+        appDatabase = Room.inMemoryDatabaseBuilder(
+                InstrumentationRegistry.getContext(),
+                AppDatabase.class
+        ).build();
+    }
+
+    @After
+    public void close(){
+        appDatabase.close();
+    }
 }
