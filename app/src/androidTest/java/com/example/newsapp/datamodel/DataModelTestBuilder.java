55a9a96b07eca278cdd88c6c9c82733931be98db
swanjala <wanjala.sam.s@gmail.com>
2019-04-03 01:24:41 +0300

    (add): data model test builder
    - Mocks the data model used for testing

--- app/src/androidTest/java/com/example/newsapp/datamodel/DataModelTestBuilder.java
+++ app/src/androidTest/java/com/example/newsapp/datamodel/DataModelTestBuilder.java
@@ -1,4 +1,26 @@
 package com.example.newsapp.datamodel;
 
+import data.datamodels.Articles;
+
 public class DataModelTestBuilder {
+
+    public static Articles createArticle(String author,
+                                         String title,
+                                         String description,
+                                         String url,
+                                         String urlToImage,
+                                         String publishedAt,
+                                         String content){
+
+        Articles articles = new Articles();
+
+        articles.setAuthor(author);
+        articles.setTitle(title);
+        articles.setDescription(description);
+        articles.setUrl(url);
+        articles.setUrlToImage(urlToImage);
+        articles.setPublishedAt(publishedAt);
+        articles.setContent(content);
+        return new Articles();
+    }
 }
