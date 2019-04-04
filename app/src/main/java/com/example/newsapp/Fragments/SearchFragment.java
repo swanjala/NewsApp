93addc5d774f51b1abe3234efbec282bf77f8ff5
swanjala <wanjala.sam.s@gmail.com>
2019-04-04 17:31:59 +0300

    (feat): Add a search fragment
    - Implement user input
    - Implement a navigation handler
    - Handle navigation using data flags

--- app/src/main/java/com/example/newsapp/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/newsapp/Fragments/SearchFragment.java
@@ -1,4 +1,99 @@
 package com.example.newsapp.Fragments;
 
-public class SearchFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.newsapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SearchFragment extends Fragment {
+
+
+    @BindView(R.id.et_search_parameter)
+    EditText et_search_parameter;
+
+    @BindView(R.id.bt_search)
+    Button bt_search;
+
+    private static String DATAFLAG;
+    private int fragmentContainer;
+    private FragmentManager fragmentManager;
+
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState){
+        View view = layoutInflater
+                .inflate(R.layout.fragment_search_news_items,
+                        container,false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+
+        bt_search.setOnClickListener(v -> {
+            String queryValue = et_search_parameter.getText().toString();
+            navigationHandler("Title Search",queryValue);
+        });
+
+
+    }
+
+    private void navigationHandler(String valueType, String queryValue){
+
+        MainFragment mainFragment = new MainFragment();
+        Bundle bundle = new Bundle();
+
+        if (valueType.equals("Domain Search")){
+
+            this.DATAFLAG = "domain_search";
+
+            bundle.putString("DataFlag", DATAFLAG);
+            bundle.putString("QueryValue", queryValue);
+            mainFragment.setArguments(bundle);
+
+            fragmentContainer = R.id.fr_main_holder;
+            fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(fragmentContainer, mainFragment)
+                    .commitAllowingStateLoss();
+        }
+        else if (valueType.trim().equals("Title Search")){
+
+            this.DATAFLAG = "title_search";
+            Log.d("Title search",queryValue);
+
+            bundle.putString("DataFlag", DATAFLAG);
+            bundle.putString("QueryValue", queryValue);
+            mainFragment.setArguments(bundle);
+
+            fragmentContainer = R.id.fr_main_holder;
+            fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(fragmentContainer, mainFragment)
+                    .commitAllowingStateLoss();
+
+
+        } else {
+            Toast.makeText(getContext(),
+                    "Search value cannot be empty",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
 }
