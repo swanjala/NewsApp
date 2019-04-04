a3a76b3068cffd5df8c8e94a184fb4ca0d3cc5b9
swanjala <wanjala.sam.s@gmail.com>
2019-04-04 18:18:55 +0300

    (refactor): Data manager flow and handling
    - Implement a separate constructor signature for querying
    - Add conditionality to check for a query to determine which
    network call block to run

--- app/src/main/java/data/database/DatabaseDataManager.java
+++ app/src/main/java/data/database/DatabaseDataManager.java
@@ -14,70 +14,84 @@ import retrofit2.Response;
 
 public class DatabaseDataManager extends AsyncTask<Void, Void,Void> {
 
-    private final ArticleAccessObject articleAccessObject;
-    private final SourcesAccessObject sourcesAccessObject;
+    private  ArticleAccessObject articleAccessObject;
+    private  SourcesAccessObject sourcesAccessObject;
 
     private ApiManager apiManager;
 
     private  Context context;
+    private String query;
 
-    public DatabaseDataManager(Context context, AppDatabase appDatabase) {
+    public DatabaseDataManager(Context context, AppDatabase appDatabase, String query) {
         articleAccessObject = appDatabase.articleAccessObject();
         sourcesAccessObject = appDatabase.sourcesAccessObject();
         this.context= context;
+        this.query = query;
+    }
+
+    public DatabaseDataManager(Context context, AppDatabase appDatabase){
+        this.context = context;
+        sourcesAccessObject = appDatabase.sourcesAccessObject();
     }
 
     @Override
     protected Void doInBackground(final Void... params) {
-        articleAccessObject.createDataIfNotExists();
+        if (query == null){
+            apiManager = new ApiManager(context);
 
+            Call<DataResponse> sourceCall = apiManager.getSources();
+            sourceCall.enqueue(new Callback<DataResponse>() {
+                @Override
+                public void onResponse(Call<DataResponse> call, Response<DataResponse> sourceResponse) {
+                    Log.d("Response sources data", String.valueOf(sourceResponse.body().getSources()));
 
-        apiManager = new ApiManager(context);
-        Call<DataResponse> call = apiManager.getArticles();
-        Call<DataResponse> sourceCall = apiManager.getSources();
-
-        call.enqueue(new Callback<DataResponse>() {
-            @Override
-            public void onResponse(Call<DataResponse> call, Response<DataResponse> response) {
-
-
-                if (response.body() != null) {
-
-                    /* Consider removing loop*/
-
-                    for (int i = 0; i < response.body().getArticles().size(); i++) {
-
-                        articleAccessObject.createDataIfNotExists(response.body().getArticles().get(i));
+                    if (sourceResponse.body() != null){
+                        for (int index = 0; index < sourceResponse.body().getSources().size() ; index++) {
+                            sourcesAccessObject.createSourceDataIfNotExists(sourceResponse.body()
+                                    .getSources().get(index));
+                        }
                     }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<DataResponse> call, Throwable t) {
-                Log.d("Data", t.getLocalizedMessage());
+                @Override
+                public void onFailure(Call<DataResponse> call, Throwable t) {
+                    Log.d("Sources Error", t.getLocalizedMessage());
+                }
+            });
 
-            }
-        });
 
-        sourceCall.enqueue(new Callback<DataResponse>() {
-            @Override
-            public void onResponse(Call<DataResponse> call, Response<DataResponse> sourceResponse) {
-                Log.d("Response sources data", String.valueOf(sourceResponse.body().getSources()));
 
-                if (sourceResponse.body() != null){
-                    for (int index = 0; index < sourceResponse.body().getSources().size() ; index++) {
-                        sourcesAccessObject.createSourceDataIfNotExists(sourceResponse.body()
-                                .getSources().get(index));
+        } else {
+
+            Log.d("datbase manafer",query);
+
+            apiManager = new ApiManager(context, query);
+            Call<DataResponse> call = apiManager.getArticles();
+            call.enqueue(new Callback<DataResponse>() {
+                @Override
+                public void onResponse(Call<DataResponse> call, Response<DataResponse> response) {
+
+
+                    if (response.body() != null) {
+
+                        /* Consider removing loop*/
+
+                        Log.d("Data gotten:", String.valueOf(response.body()));
+
+                        for (int i = 0; i < response.body().getArticles().size(); i++) {
+
+                            articleAccessObject.createDataIfNotExists(response.body().getArticles().get(i));
+                        }
                     }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<DataResponse> call, Throwable t) {
-                Log.d("Sources Error", t.getLocalizedMessage());
-            }
-        });
+                @Override
+                public void onFailure(Call<DataResponse> call, Throwable t) {
+                    Log.d("Data", t.getLocalizedMessage());
 
+                }
+            });
+        }
         return null;
     }
 }
