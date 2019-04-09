e8dc54f43777b55eb6753d50a068562211e8a447
swanjala <wanjala.sam.s@gmail.com>
2019-04-09 22:50:33 +0300

    (chore): Update gradle default config
    - Enable multidex
    - Implement preference library

---
+++ app/src/main/java/com/example/newsapp/Fragments/SettingsFragment.java
@@ -0,0 +1,18 @@
+package com.example.newsapp.Fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.example.newsapp.R;
+
+public class SettingFragment extends PreferenceFragmentCompat {
+
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+
+        addPreferencesFromResource(R.xml.pref_high_contrast);
+
+
+    }
+}
