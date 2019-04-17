5682dd988c17c8a661398ef1ef334eec28259b59
swanjala <wanjala.sam.s@gmail.com>
2019-04-17 05:39:00 +0300

    (update): news view model
    - check if articles by country is null
    - clear values from previous query results

--- app/src/main/java/viewmodels/NewsViewModel.java
+++ app/src/main/java/viewmodels/NewsViewModel.java
@@ -27,9 +27,8 @@ public class NewsViewModel extends AndroidViewModel {
     private LiveData<List<Articles>> newsBySetToRead;
     private LiveData<List<Articles>> articlesByDomain;
     private LiveData<List<Articles>> articlesByTitle;
-    private LiveData<List<Articles>> articlesByCountry;
+    private List<Articles> articlesByCountry;
     private LiveData<List<Sources>> sourcesByNewsCategory;
-    private String queryParams;
     private Context context;
 
     public NewsViewModel (Application application){
@@ -65,7 +64,11 @@ public class NewsViewModel extends AndroidViewModel {
     public LiveData<List<String>> fetchAllCountries() {
         return countryLiveData;
     }
-    public LiveData<List<Articles>> fetchArticlesByCountry(String country){
+    public List<Articles> fetchArticlesByCountry(String country){
+
+        if (articlesByCountry != null) {
+            articlesByCountry.clear();
+        }
 
         articlesByCountry  = mRepository.getArticlesByCountry(context,country);
 
