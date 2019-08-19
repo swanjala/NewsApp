77d58b1c06b16c9191187a8c1b995b66f445893e
live-beta <wanjala.sam.s@gmail.com>
2019-08-20 00:05:38 +0300

    (update): Main kotlin activity

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.newsapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.FrameLayout
+import com.example.newsapp.Fragments.CategoriesFragment
 import com.example.newsapp.Fragments.MainFragment
+import com.example.newsapp.Fragments.SourcesFragment
 import com.google.firebase.analytics.FirebaseAnalytics
 import java.lang.invoke.ConstantCallSite
 
@@ -89,16 +96,89 @@ class MainActivity: AppCompatActivity(){
         var bundle:Bundle = Bundle()
         bundle.putString(resources.getString(R.string.data_flag_key), DATAFLAG)
 
-        mainFragment.arguments()
+        mainFragment.arguments = bundle
+
+        if (LARGE_SCREEN_SIZE && findViewById<FrameLayout>(R.id.frame_articles) != null){
+            fragmentContainer = R.id.frame_articles
+        }else if (findViewById<FrameLayout>(R.id.fr_main_holder) != null){
+            fragmentContainer = R.id.fr_main_holder
+        }
+        if(fragmentContainer.toString().equals(0)){
+            fragmentManager = this.supportFragmentManager
+            fragmentManager.beginTransaction()
+                    .replace(fragmentContainer, mainFragment)
+                    .commitAllowingStateLoss()
+        }
     }
 
     fun loadSourcesList(){
 
+        this.DATAFLAG = getString(R.string.load_all_data_flag)
+
+        var sourcesFragment = SourcesFragment()
+        var bundle:Bundle = Bundle()
+        bundle.putString("sourceCategory","")
+        sourcesFragment.arguments = bundle
+
+        if(LARGE_SCREEN_SIZE){
+            fragmentContainer = R.id.frame_articles
+        }else {
+            fragmentContainer = R.id.fr_main_holder
+        }
+        fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction()
+                .replace(fragmentContainer, sourcesFragment)
+                .commitAllowingStateLoss()
+
     }
     fun loadCountries(){
-
+        var intent = Intent(this, HeadlinesByCountry::class.java)
+        startActivity(intent)
     }
 
+    fun loadCategories() {
+        var categoriesFragment:CategoriesFragment = CategoriesFragment()
+        fragmentContainer = R.id.fr_main_holder
+        fragmentManager = this.supportFragmentManager
+        fragmentManager.beginTransaction()
+                .replace(fragmentContainer, categoriesFragment)
+                .commitAllowingStateLoss()
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater:MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        return when (item?.itemId){
+            R.id.all_news ->{
+                loadData()
+                 true
+            }
+            R.id.news_sources -> {
+                loadSourcesList()
+                 true
+            }
+            R.id.news_categories -> {
+                loadCategories()
+                 true
+            }
+            R.id.loadFavorites -> {
+                this.DATAFLAG = getString(R.string.load_favorites_flag);
+                loadData()
+                 true
+            }
+            R.id.action_settings -> {
+                var startSettingIntent = Intent(this, Settings::class.java)
+                startActivity(startSettingIntent)
+                true
+
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 }
