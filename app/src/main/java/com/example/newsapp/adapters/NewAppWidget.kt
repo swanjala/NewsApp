e4a65fe3469f86b178011402fe368f0666eb1b4e
live-beta <wanjala.sam.s@gmail.com>
2019-08-20 01:50:22 +0300

    (add): app widget file'

---
+++ app/src/main/java/com/example/newsapp/adapters/NewAppWidget.kt
@@ -0,0 +1,13 @@
+package com.example.newsapp.adapters
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class NewAppWidget : AppWidgetProvider() {
+
+    companion object updateAppWidget(context: Context, appWidgetManager:AppWidgetManager,
+                                     appWidgetId:Int) {
+
+    }
+}
