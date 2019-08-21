23c783635ddb2a8b1a76c80055b618f77d5c51aa
live-beta <wanjala.sam.s@gmail.com>
2019-08-21 20:45:29 +0300

    (update): add the update widget and delete widget

--- app/src/main/java/com/example/newsapp/adapters/NewAppWidget.kt
+++ app/src/main/java/com/example/newsapp/adapters/NewAppWidget.kt
@@ -2,20 +2,37 @@ package com.example.newsapp.adapters
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.arch.lifecycle.LiveData
 import android.content.Context
+import android.os.AsyncTask
+import android.widget.RemoteViews
+import com.example.newsapp.R
+import data.datamodels.Articles
 
 class NewAppWidget : AppWidgetProvider() {
 
-    companion object updateAppWidget(context: Context, appWidgetManager:AppWidgetManager,
-                                     appWidgetId:Int) {
+    fun  updateAppWidget(context: Context, appWidgetManager:AppWidgetManager,
+                                     appWidgetId:Int): Unit{
+        var remoteViews = RemoteViews(context.packageName, R.layout.new_app_widget)
 
+//        AsyncTask<Context, Void, LiveData<List<Articles>>> () {
+//        }
     }
 
     override fun onUpdate(context:Context,
                           appWidgetManager: AppWidgetManager,
                           appWidgetIds: Int[]){
         for (appWidgetId:Int, appWidgetIds ){
+            updateAppWidget(context, appWidgetManager , appWidgetId)
 
         }
     }
+
+    override fun onDeleted(context:Context, appWidgetIds:Int[]){
+        for (appWidgetId:Int : appWidgetId){
+            NewAppWidgetConfigureActivity.deleteTitlePref(context, appwidgetId);
+        }
+    }
+
+
 }
