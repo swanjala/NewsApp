e1b6a841a36a1140226caa9203c174bd79ecf881
live-beta <wanjala.sam.s@gmail.com>
2019-08-21 23:13:58 +0300

    (add): main adapter
    - Add construcor delaration
    - Add initialization block
    - Implement ViewHolder class

--- app/src/main/java/com/example/newsapp/adapters/MainAdapter.kt
+++ app/src/main/java/com/example/newsapp/adapters/MainAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.newsapp.adapters
 
-class MainAdapter {
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import data.datamodels.Articles
+import java.util.*
+
+class MainAdapter(context: Context, articlesList:List<Articles>):
+        RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
+
+    private var layoutInflater:LayoutInflater
+
+    init {
+
+        Collections.sort(articlesList) { o1, o2 -> o1.publishedAt.compareTo(o2.publishedAt) }
+
+        layoutInflater = LayoutInflater.from(context)
+    }
+
+    class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+        lateinit var tvTitle:TextView
+
+        lateinit var description:TextView
+        lateinit var tv_author:TextView
+        lateinit var tv_date:TextView
+
+    }
+
 }
