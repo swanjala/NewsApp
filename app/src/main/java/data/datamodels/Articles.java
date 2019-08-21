f2cb7f10646e5d613401664d2ca46ea1f18189d2
live-beta <wanjala.sam.s@gmail.com>
2019-08-22 01:07:46 +0300

    (add): articles data access objects

--- app/src/main/java/data/datamodels/Articles.java
+++ app/src/main/java/data/datamodels/Articles.java
@@ -18,131 +18,131 @@ import data.database.utils.NewsAppTypeConverter;
 
 public class Articles {
 
-        @SerializedName("id")
-        @Expose
-        @PrimaryKey(autoGenerate = true)
-        private int id;
+    @SerializedName("id")
+    @Expose
+    @PrimaryKey(autoGenerate = true)
+    private int id;
 
-        @SerializedName("author")
-        @Expose
-        private String author;
-        @SerializedName("title")
-        @Expose
-        private String title;
-        @SerializedName("description")
-        @Expose
-        private String description;
-        @SerializedName("url")
-        @Expose
-        private String url;
-        @SerializedName("urlToImage")
-        @Expose
-        private String urlToImage;
+    @SerializedName("author")
+    @Expose
+    private String author;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("urlToImage")
+    @Expose
+    private String urlToImage;
 
-        @SerializedName("publishedAt")
-        @Expose
-        private String publishedAt;
-        @SerializedName("content")
-        @Expose
-        private String content;
+    @SerializedName("publishedAt")
+    @Expose
+    private String publishedAt;
+    @SerializedName("content")
+    @Expose
+    private String content;
 
-        @SerializedName("country")
-        @Expose
-        private String country;
+    @SerializedName("country")
+    @Expose
+    private String country;
 
-         @SerializedName("toRead")
-        @Expose
-        private boolean toRead;
+    @SerializedName("toRead")
+    @Expose
+    private boolean toRead;
 
-        @SerializedName("favorite")
-        @Expose
-        private boolean favorite;
+    @SerializedName("favorite")
+    @Expose
+    private boolean favorite;
 
-        public boolean getFavorite() {
-            return favorite;
-        }
+    public boolean getFavorite() {
+        return favorite;
+    }
 
-        public void setFavorite(boolean favorite) {
-            this.favorite = favorite;
-        }
-        public boolean getToRead() {
-            return toRead;
-        }
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+    public boolean getToRead() {
+        return toRead;
+    }
 
-        public void setToRead(boolean toRead) {
-            this.toRead = toRead;
-        }
+    public void setToRead(boolean toRead) {
+        this.toRead = toRead;
+    }
 
-        public String getCountry() {
-            return country;
-        }
+    public String getCountry() {
+        return country;
+    }
 
-        public void setCountry(String country) {
-            this.country = country;
-        }
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-        public String getAuthor() {
-            return author;
-        }
+    public String getAuthor() {
+        return author;
+    }
 
-        public void setAuthor(String author) {
-            this.author = author;
-        }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-        public String getTitle() {
-            return title;
-        }
+    public String getTitle() {
+        return title;
+    }
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-        public String getDescription() {
-            return description;
-        }
+    public String getDescription() {
+        return description;
+    }
 
-        public void setDescription(String description) {
-            this.description = description;
-        }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-        public String getUrl() {
-            return url;
-        }
+    public String getUrl() {
+        return url;
+    }
 
-        public void setUrl(String url) {
-            this.url = url;
-        }
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-        public String getPublishedAt() {
-            return publishedAt;
-        }
+    public String getPublishedAt() {
+        return publishedAt;
+    }
 
-        public void setPublishedAt(String publishedAt) {
-            this.publishedAt = publishedAt;
-        }
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
 
-        public String getContent() {
-            return content;
-        }
+    public String getContent() {
+        return content;
+    }
 
-        public void setContent(String content) {
-            this.content = content;
-        }
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-        public int getId() {
-            return id;
-        }
+    public int getId() {
+        return id;
+    }
 
-        public void setId(int id) {
-            this.id = id;
-        }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        public String getUrlToImage() {
-            return urlToImage;
-        }
-        public void setUrlToImage(String urlToImage) {
-            this.urlToImage = urlToImage;
-        }
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
 
 
 }
---
+++ app/src/main/java/data/datamodels/Articles.kt
@@ -0,0 +1,19 @@
+package data.datamodels
+
+import android.arch.persistence.room.*
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import data.database.utils.NewsAppTypeConverter
+
+@Entity(tableName = "articles")
+
+@TypeConverters(NewsAppTypeConverter::class)
+data class Articles(@PrimaryKey(autoGenerate = true) var id: Int?) {
+
+    @ColumnInfo(name = "author") var author:String,
+
+
+
+
+}
