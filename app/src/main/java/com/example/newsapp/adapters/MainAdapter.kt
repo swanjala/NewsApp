d8abd04a69df5f703eb981fa53710ecdeddee9e5
live-beta <wanjala.sam.s@gmail.com>
2019-08-21 23:20:00 +0300

    --ammend

--- app/src/main/java/com/example/newsapp/adapters/MainAdapter.kt
+++ app/src/main/java/com/example/newsapp/adapters/MainAdapter.kt
@@ -28,12 +28,12 @@ class MainAdapter(context: Context, articlesList:List<Articles>):
 
         var tvTitle:TextView = view.findViewById(R.id.tv_title)
 
-         var description:TextView = view.findViewById(R.id.tv_description)
-         var tv_author:TextView = view.findViewById(R.id.tv_author)
-         var tv_date:TextView = view.findViewById(R.id.tv_date)
-         var iv_articleImage:ImageView = view.findViewById(R.id.tv_date)
+        var description:TextView = view.findViewById(R.id.tv_description)
+        var tv_author:TextView = view.findViewById(R.id.tv_author)
+        var tv_date:TextView = view.findViewById(R.id.tv_date)
+        var iv_articleImage:ImageView = view.findViewById(R.id.tv_date)
         var publishedAt:String = ""
-         lateinit var userActionViewModel: NewsViewModel
+        lateinit var userActionViewModel: NewsViewModel
         var contrastFlag:Boolean = false
 
 
