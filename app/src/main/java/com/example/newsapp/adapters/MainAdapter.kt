bb0a5faea23befff39464e0d2dadd88ad5d91e44
live-beta <wanjala.sam.s@gmail.com>
2019-08-21 23:27:30 +0300

    (add): high contrast flag for usability

--- app/src/main/java/com/example/newsapp/adapters/MainAdapter.kt
+++ app/src/main/java/com/example/newsapp/adapters/MainAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.newsapp.adapters
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -11,11 +13,13 @@ import data.datamodels.Articles
 import kotlinx.android.synthetic.main.card_news_data.view.*
 import viewmodels.NewsViewModel
 import java.util.*
+import kotlin.coroutines.coroutineContext
 
 class MainAdapter(context: Context, articlesList:List<Articles>):
         RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
 
     private var layoutInflater:LayoutInflater
+    private var mContext = context
 
     init {
 
@@ -24,11 +28,11 @@ class MainAdapter(context: Context, articlesList:List<Articles>):
         layoutInflater = LayoutInflater.from(context)
     }
 
-    class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+    class MyViewHolder(var view: View): RecyclerView.ViewHolder(view){
 
-        var tvTitle:TextView = view.findViewById(R.id.tv_title)
+        var tv_title:TextView = view.findViewById(R.id.tv_title)
 
-        var description:TextView = view.findViewById(R.id.tv_description)
+        var tv_description:TextView = view.findViewById(R.id.tv_description)
         var tv_author:TextView = view.findViewById(R.id.tv_author)
         var tv_date:TextView = view.findViewById(R.id.tv_date)
         var iv_articleImage:ImageView = view.findViewById(R.id.tv_date)
@@ -36,7 +40,17 @@ class MainAdapter(context: Context, articlesList:List<Articles>):
         lateinit var userActionViewModel: NewsViewModel
         var contrastFlag:Boolean = false
 
+        fun setData(current:Articles, position:Int){
+            publishedAt = current.publishedAt
+            tv_title.setText(current.title)
+            this.tv_description.setText(current.description)
 
+            var sharedPreferences:SharedPreferences =
+                    PreferenceManager.getDefaultSharedPreferences(view.context)
+            contrastFlag =sharedPreferences
+                    .getBoolean(view.context.getString(R.string.high_contrast_key),false)
+
+        }
 
     }
 
