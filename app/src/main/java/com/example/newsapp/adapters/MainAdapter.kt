d164bbbc9bd6d7d5a65be015e5551a4bee3b84bc
live-beta <wanjala.sam.s@gmail.com>
2019-08-21 23:19:23 +0300

    (add): view declaration and assignment

--- app/src/main/java/com/example/newsapp/adapters/MainAdapter.kt
+++ app/src/main/java/com/example/newsapp/adapters/MainAdapter.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import com.example.newsapp.R
 import data.datamodels.Articles
+import kotlinx.android.synthetic.main.card_news_data.view.*
+import viewmodels.NewsViewModel
 import java.util.*
 
 class MainAdapter(context: Context, articlesList:List<Articles>):
@@ -22,11 +26,17 @@ class MainAdapter(context: Context, articlesList:List<Articles>):
 
     class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
 
-        lateinit var tvTitle:TextView
+        var tvTitle:TextView = view.findViewById(R.id.tv_title)
 
-        lateinit var description:TextView
-        lateinit var tv_author:TextView
-        lateinit var tv_date:TextView
+         var description:TextView = view.findViewById(R.id.tv_description)
+         var tv_author:TextView = view.findViewById(R.id.tv_author)
+         var tv_date:TextView = view.findViewById(R.id.tv_date)
+         var iv_articleImage:ImageView = view.findViewById(R.id.tv_date)
+        var publishedAt:String = ""
+         lateinit var userActionViewModel: NewsViewModel
+        var contrastFlag:Boolean = false
+
+
 
     }
 
