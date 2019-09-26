35b5a21142464c2961007e07b2d51efc2895c1c8
live-beta <wanjala.sam.s@gmail.com>
2019-09-26 19:28:33 +0300

    (update): database initializer

---
+++ app/src/androidTest/java/com/example/newsapp/database/DatabaseTest.kt
@@ -0,0 +1,27 @@
+package com.example.newsapp.database
+
+import android.arch.persistence.room.Room
+import android.support.test.InstrumentationRegistry
+
+import org.junit.After
+import org.junit.Before
+
+import com.example.newsapp.data.database.AppDatabase
+
+open class DatabaseTest {
+
+    protected var appDatabase: AppDatabase
+
+    @Before
+    fun init() {
+        appDatabase = Room.inMemoryDatabaseBuilder(
+                InstrumentationRegistry.getContext(),
+                AppDatabase::class.java
+        ).build()
+    }
+
+    @After
+    fun close() {
+        appDatabase.close()
+    }
+}
