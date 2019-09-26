2c964564b3a4f4a4144d87034a422bc037b91c44
live-beta <wanjala.sam.s@gmail.com>
2019-09-26 19:33:21 +0300

    (update): news app instanciator

---
+++ app/src/androidTest/java/com/example/newsapp/TestNewsApp.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp
+
+import android.app.Application
+
+class TestNewsApp : Application() {
+
+    //Create test application instance
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
