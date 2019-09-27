9cfb44f87db8cbe398634f440d38a0e2a18f9bed
live-beta <wanjala.sam.s@gmail.com>
2019-09-27 19:18:25 +0300

    (add): main data respository implementation
    - Implement a sources resource based data execution

--- app/src/main/java/com/example/newsapp/data/DataRepository/MainDataRepositoryImpl.kt
+++ app/src/main/java/com/example/newsapp/data/DataRepository/MainDataRepositoryImpl.kt
@@ -2,12 +2,19 @@ package com.example.newsapp.data.DataRepository
 
 import android.app.Application
 import android.arch.lifecycle.LiveData
+import com.example.newsapp.data.DataRepository.repositorymodel.Source
+import com.example.newsapp.data.DataRepository.utils.AppExecutor
 import com.example.newsapp.data.api.NetworkService
 import com.example.newsapp.data.database.AppDatabase
 import com.example.newsapp.data.database.accessobjects.ArticleAccessObject
 import com.example.newsapp.data.database.accessobjects.SourcesAccessObject
 import com.example.newsapp.data.datamodels.Articles
 import com.example.newsapp.data.datamodels.Sources
+import com.example.newsapp.data.DataRepository.utils.Resource
+import com.example.newsapp.data.DataRepository.utils.ResourceHandler
+import com.example.newsapp.data.api.ApiResponse
+import com.example.newsapp.data.api.SourcesResponse
+
 import javax.inject.Inject
 import javax.inject.Singleton
 /*TODO
@@ -16,7 +23,10 @@ import javax.inject.Singleton
 class MainDataRepositoryImpl @Inject constructor(networkService: NetworkService,
                                                  appDatabase: AppDatabase,
                                                  articlesAccessObject: ArticleAccessObject,
-                                                 sourcesAccessObject: SourcesAccessObject):MainDataRepository{
+                                                 sourcesAccessObject: SourcesAccessObject
+                                                appExecutor: AppExecutor):MainDataRepository{
+    private var mAppExecutors = appExecutor
+    private var mNetworkService = networkService
 
     private lateinit var articlesDataAccessObject: ArticleAccessObject
     private lateinit var sourcesAccessObject: SourcesAccessObject
@@ -43,6 +53,26 @@ class MainDataRepositoryImpl @Inject constructor(networkService: NetworkService,
         return articlesAccessObject.fetchAllData()
     }
 
+    fun getFetchNewsSource():LiveData<Resource<List<Sources>>>{
+
+    }
+
+    val allSources: LiveData<Resource<List<Sources>>>
+        get() = object :ResourceHandler<List<Sources>, SourcesResponse>(mAppExecutors){
+
+            override fun saveCallResult(item:SourcesResponse){
+                sourcesAccessObject.createSourceDataIfNotExists(item.results)
+            }
+            override fun shouldFetch(data:List<Sources>?): Boolean{
+                return data == null || data.isEmpty()
+            }
+            override fun loadFromDb():LiveData<List<Sources>>{
+                return sourcesAccessObject.fetchCategoryList()
+            }
+            override fun createCall():LiveData<ApiResponse<SourcesResponse>>{
+                return mNetworkService.getSources("")
+            }
+    }.asLiveData()
 
 
 
