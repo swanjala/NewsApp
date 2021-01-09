0b7a1d0a344df8b2a18a726dd66286ee0d36ee42
Sam Wanjala <wanjala.sam.s@gmail.com>
2021-01-09 22:03:27 +0100

    Fix-up: Update live data items for the news view model

--- app/src/main/java/com/example/newsapp/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/viewmodels/NewsViewModel.kt
@@ -3,28 +3,65 @@ package com.example.newsapp.viewmodels
 import android.annotation.SuppressLint
 import android.app.Application
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.content.Context
 import com.example.newsapp.data.DataRepository.ArticleRepository
 import com.example.newsapp.data.datamodels.Articles
 import com.example.newsapp.data.datamodels.Sources
 import javax.inject.Inject
+import javax.xml.transform.Source
 
 
 class NewsViewModel(application: Application) : ViewModel() {
 
     internal var mRepository: ArticleRepository
 
-    private val articlesLiveData: LiveData<List<Articles>>?
-    private val articlesNoQueryLiveData: LiveData<List<Articles>>?
-    private val sourcesLiveData: LiveData<List<Sources>>?
-    private var countryLiveData: LiveData<List<String>>? = null
-    private val newsCategories: LiveData<List<String>>?
-    private var newsByFavorite: LiveData<List<Articles>>? = null
-    private var newsBySetToRead: LiveData<List<Articles>>? = null
-    private var articlesByDomain: LiveData<List<Articles>>? = null
-    private var articlesByCountry: LiveData<List<Articles>>? = null
-    private var sourcesByNewsCategory: LiveData<List<Sources>>? = null
+    private val _currentChartPagerItem = MutableLiveData<Int>().apply { value = 0 }
+    val currentChartPagerItem: LiveData<Int>
+        get() = _currentChartPagerItem
+
+
+    private val _articleLiveData = MutableLiveData<List<Articles>>()
+    val articlesLiveData: LiveData<List<Articles>>?
+        get() = _articleLiveData
+
+    private val _articlesNoQueryLiveData = MutableLiveData<List<Articles>>()
+    val articlesNoQueryLiveData: LiveData<List<Articles>>
+        get() = _articlesNoQueryLiveData
+
+
+    private val _sourcesLiveData = MutableLiveData<List<Sources>>()
+    val sourcesLiveData: LiveData<List<Source>>
+        get() = _sourcesLiveData
+
+    private val _countryLiveData = MutableLiveData<List<String>>()
+    val countryLiveData: LiveData<List<String>>
+        get() = _countryLiveData
+
+    private val _newCategories = MutableLiveData<List<String>>()
+    val newCategories: LiveData<List<String>>
+        get() = _newCategories
+
+    private val _newsByFavorite = MutableLiveData<List<Articles>>()
+    val newsByFavorite: LiveData<List<Articles>>
+        get() = _newsByFavorite
+
+    private val _newsBySetToRead = MutableLiveData<List<Articles>>()
+    val newsBySetToRead: LiveData<List<Articles>>
+        get() = _newsBySetToRead
+
+    private val _articlesByDomain = MutableLiveData<List<Articles>>()
+    val articlesByDomain: LiveData<List<Articles>>
+        get() = _articlesByDomain
+
+    private var _articlesByCountry = MutableLiveData<List<Articles>>()
+    val articlesByCountry: LiveData<List<Articles>>
+        get() = _articlesByCountry
+
+    private var _sourcesByNewsCategory = MutableLiveData<List<Sources>>()
+        val sourcesByNewsCategory:LiveData<List<Sources>>
+    get() = _sourcesByNewsCategory
 
     @SuppressLint("StaticFieldLeak")
     @Inject
@@ -77,7 +114,7 @@ class NewsViewModel(application: Application) : ViewModel() {
 
     fun fetchDataByNewsCategories(categoryQuery: String): LiveData<List<Sources>> {
 
-        return  mRepository.getSourcesByNewsCategory(categoryQuery)
+        return mRepository.getSourcesByNewsCategory(categoryQuery)
     }
 
     fun fetchDataByFavorite(): LiveData<List<Articles>> {
