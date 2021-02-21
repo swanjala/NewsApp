7ab253872702b7166120d6c3c64a78e06f9dbfae
Sam Wanjala <wanjala.sam.s@gmail.com>
2021-02-21 18:54:46 +0100

    feature/refactor: simplify comparisons and update property references

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -33,31 +33,12 @@ class MainActivity : AppCompatActivity() {
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
 
         if (findViewById<ConstraintLayout>(R.id.largeScreen) != null) {
-            this.LARGE_SCREEN_SIZE = true
+            LARGE_SCREEN_SIZE = true
 
-            if (DATAFLAG != null) {
-
-                if (DATAFLAG.equals(getString(R.string.load_all_data_flag))) {
-                    loadData()
-                } else if (DATAFLAG.equals(getString(R.string.load_data_sources_flag))) {
-                    loadSourcesList()
-                }
-            } else if (findViewById<ConstraintLayout>(R.id.cl_country_detail) != null) {
-                this.LARGE_SCREEN_SIZE = true
-                loadCountries()
-            } else if (findViewById<FrameLayout>(R.id.fr_main_holder) != null) {
-                this.LARGE_SCREEN_SIZE = false;
-
-                if (DATAFLAG != null) {
-
-                    if (DATAFLAG != null && DATAFLAG.equals(getString(R.string.load_data_sources_flag))) {
-                        loadData()
-                    } else if (DATAFLAG.equals(getString(R.string.load_data_sources_flag))) {
-                        loadSourcesList()
-                    }
-                } else {
-                    loadSourcesList()
-                }
+            if (DATAFLAG.equals(getString(R.string.load_all_data_flag))) {
+                loadData()
+            } else if (DATAFLAG.equals(getString(R.string.load_data_sources_flag))) {
+                loadSourcesList()
             }
         }
     }
@@ -66,28 +47,24 @@ class MainActivity : AppCompatActivity() {
 
         super.onRestoreInstanceState(savedInstanceState)
         if (findViewById<ConstraintLayout>(R.id.largeScreen) != null) {
-            this.LARGE_SCREEN_SIZE = true
-            if (DATAFLAG != null) {
-                if (DATAFLAG.equals(getString(R.string.load_all_data_flag))) {
-                    loadData();
-                } else if (DATAFLAG.equals(getString(R.string.load_data_sources_flag))) {
-                    loadSourcesList()
-                }
+            LARGE_SCREEN_SIZE = true
+            if (DATAFLAG.equals(getString(R.string.load_all_data_flag))) {
+                loadData();
+            } else if (DATAFLAG.equals(getString(R.string.load_data_sources_flag))) {
+                loadSourcesList()
             }
         } else if (findViewById<FrameLayout>(R.id.fr_main_holder) != null) {
-            this.LARGE_SCREEN_SIZE = false;
+            LARGE_SCREEN_SIZE = false;
 
-            if (DATAFLAG != null) {
-                if (DATAFLAG.equals(getString(R.string.load_all_data_flag))) {
-                    loadData()
-                }
+            if (DATAFLAG.equals(getString(R.string.load_all_data_flag))) {
+                loadData()
             }
         }
         if (findViewById<ConstraintLayout>(R.id.cl_country_detail) != null) {
-            this.LARGE_SCREEN_SIZE = true
+            LARGE_SCREEN_SIZE = true
             loadCountries()
         }
-        this.LARGE_SCREEN_SIZE = false
+        LARGE_SCREEN_SIZE = false
     }
 
     fun loadData() {
@@ -113,7 +90,7 @@ class MainActivity : AppCompatActivity() {
 
     fun loadSourcesList() {
 
-        this.DATAFLAG = getString(R.string.load_all_data_flag)
+        DATAFLAG = getString(R.string.load_all_data_flag)
 
         val sourcesFragment = SourcesFragment()
         val bundle: Bundle = Bundle()
@@ -168,7 +145,7 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             R.id.loadFavorites -> {
-                this.DATAFLAG = getString(R.string.load_favorites_flag);
+                DATAFLAG = getString(R.string.load_favorites_flag);
                 loadData()
                 true
             }
