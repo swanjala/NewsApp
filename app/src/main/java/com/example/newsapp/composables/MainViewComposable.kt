c39cdbb485d3e6ed59917b705fd97ef209ec5598
Sam Stevens <samstevens@Sams-MacBook-Pro.local>
2022-11-03 18:04:39 +0100

    Implement image dependency

--- app/src/main/java/com/example/newsapp/composables/MainViewComposable.kt
+++ app/src/main/java/com/example/newsapp/composables/MainViewComposable.kt
@@ -1,19 +1,15 @@
 package com.example.newsapp.composables
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.LiveData
 import com.example.newsapp.network.model.News
-import com.example.newsapp.ui.theme.NewsAppTheme
 
 @Composable
 fun MainViewComposable(
