5f76e2f210db485a09ce537319af7027326666f7
Sam Stevens <samstevens@Sams-MacBook-Pro.local>
2022-11-06 18:51:10 +0100

    Update dependency injection logic

--- app/src/main/java/com/example/newsapp/dependencies/AppComponent.kt
+++ app/src/main/java/com/example/newsapp/dependencies/AppComponent.kt
@@ -3,19 +3,30 @@ package com.example.newsapp.dependencies
 
 import android.content.Context
 import com.example.newsapp.MainActivity
-import com.example.newsapp.news.module.NewsModule
+import com.example.newsapp.NewsApplication
 import dagger.BindsInstance
 import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [NewsModule::class, ViewModelModule::class])
-interface AppComponent {
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        NewsModule::class,
+        ViewModelModule::class,
+        NetworkModule::class
+    ]
+)
+interface AppComponent : AndroidInjector<DaggerApplication> {
 
     @Component.Factory
     interface Factory {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
+    fun inject(app: NewsApplication)
     fun inject(activity: MainActivity)
 }
