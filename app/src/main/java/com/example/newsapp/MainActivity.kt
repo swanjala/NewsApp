1e8fcf556b7c08bb99e8710d5d500b9620ba4a71
Sam Stevens <samstevens@Sams-MacBook-Pro.local>
2022-11-06 18:51:59 +0100

    Update network module

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.example.newsapp
 
 import android.os.Bundle
-import android.os.StrictMode
-import android.os.StrictMode.VmPolicy
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
-import com.example.newsapp.composables.MainViewComposable
+import com.example.newsapp.composables.NewsApp
 import com.example.newsapp.news.domain.NewsViewModel
-import com.example.newsapp.ui.theme.NewsAppTheme
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
@@ -24,19 +18,12 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        (application as NewsApplication).appComponent.inject(this)
+        (application as NewsApplication).initializeDaggerComponent().inject(this)
         super.onCreate(savedInstanceState)
-        StrictMode.setVmPolicy(
-            VmPolicy.Builder(StrictMode.getVmPolicy())
-                .detectLeakedClosableObjects()
-                .build()
-        )
         setContent {
-            NewsAppTheme {
-                MainViewComposable(
+                NewsApp(
                     viewModel.response
                 )
-            }
         }
     }
 }
