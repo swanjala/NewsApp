a95d98a32168f3c06c66381e1ba6c31c064f328a
Sam Stevens <samstevens@Sams-MacBook-Pro.local>
2022-11-06 18:50:37 +0100

    Add news application base activity

--- app/src/main/java/com/example/newsapp/NewsApplication.kt
+++ app/src/main/java/com/example/newsapp/NewsApplication.kt
@@ -1,12 +1,22 @@
 package com.example.newsapp
 
-import android.app.Application
-import androidx.activity.ComponentActivity
 import com.example.newsapp.dependencies.AppComponent
 import com.example.newsapp.dependencies.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
 
-open class NewsApplication : Application() {
-    val appComponent: AppComponent by lazy {
-        DaggerAppComponent.factory().create(applicationContext)
+open class NewsApplication : DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return initializeDaggerComponent()
+    }
+
+    open fun initializeDaggerComponent(): AppComponent {
+        val appComponent: AppComponent =
+            DaggerAppComponent.factory().create(applicationContext)
+
+        appComponent.inject(this)
+
+        return appComponent
     }
 }
