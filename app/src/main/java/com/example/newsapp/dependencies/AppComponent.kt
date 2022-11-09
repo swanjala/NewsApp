168164f75fb7ff10acaec6baeb50af5079f975c5
swanjala <wanjala.sam.s@gmail.com>
2022-11-09 12:01:47 +0100

    Improve app component dependency strategy

--- app/src/main/java/com/example/newsapp/dependencies/AppComponent.kt
+++ app/src/main/java/com/example/newsapp/dependencies/AppComponent.kt
@@ -1,14 +1,11 @@
 package com.example.newsapp.dependencies
 
 
-import android.content.Context
-import com.example.newsapp.MainActivity
+import android.app.Application
 import com.example.newsapp.NewsApplication
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
-import dagger.android.AndroidInjector
-import dagger.android.DaggerApplication
 import javax.inject.Singleton
 
 @Singleton
@@ -17,16 +14,19 @@ import javax.inject.Singleton
         AndroidInjectionModule::class,
         NewsModule::class,
         ViewModelModule::class,
-        NetworkModule::class
+        NetworkModule::class,
+        DatabaseModule::class,
+        MainActivityModule::class
     ]
 )
-interface AppComponent : AndroidInjector<DaggerApplication> {
+interface AppComponent {
 
-    @Component.Factory
-    interface Factory {
-        fun create(@BindsInstance context: Context): AppComponent
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+        fun build(): AppComponent
     }
 
-    fun inject(app: NewsApplication)
-    fun inject(activity: MainActivity)
+    fun inject(newsApplication: NewsApplication)
 }
