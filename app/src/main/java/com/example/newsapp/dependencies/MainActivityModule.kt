ae74d73e5bc4b3548a64a458b6d7d09b11c852ab
swanjala <wanjala.sam.s@gmail.com>
2022-11-09 12:09:19 +0100

    Add main activity module

---
+++ app/src/main/java/com/example/newsapp/dependencies/MainActivityModule.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.dependencies
+
+import com.example.newsapp.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Suppress("unused")
+@Module
+abstract class MainActivityModule {
+    @ContributesAndroidInjector
+    abstract fun contribityMainActivity():MainActivity
+}
