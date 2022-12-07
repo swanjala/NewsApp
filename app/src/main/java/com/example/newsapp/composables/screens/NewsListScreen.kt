26b749d813f15c202d263ae1d10c0f80323ed629
Sammy Wanjala <sammywanjala@Sammys-MacBook-Pro-2.local>
2022-12-07 15:45:26 +0100

    Modify news list screen

--- app/src/main/java/com/example/newsapp/composables/screens/NewsListScreen.kt
+++ app/src/main/java/com/example/newsapp/composables/screens/NewsListScreen.kt
@@ -2,36 +2,30 @@
 
 package com.example.newsapp.composables.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.example.newsapp.composables.components.ArticleCard
+import com.example.newsapp.composables.screens.screenmodels.ScreenType
 import com.example.newsapp.data.model.Article
-import com.example.newsapp.news.fragments.NewsFragmentDirections
 
 @Composable
 fun NewsListScreen(
+    screenType: ScreenType,
     articles: List<Article>?,
-    navController: NavController
+    handleArticleSelected: (ScreenType,Article) -> Unit
 ) {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
         LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(1.dp)
+            modifier = Modifier.fillMaxSize()
         ) {
             articles?.forEach { article ->
                 item {
-                    ArticleCard(article) {
-                        val action = NewsFragmentDirections.nextAction(article)
-                        navController.navigate(action)
-                    }
+                    ArticleCard(screenType,article, handleArticleSelected)
                 }
             }
         }
