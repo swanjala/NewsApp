c2de4beaf9c9c2ae67bdd6dd1ac82db6a2f85151
Sammy Wanjala <sammywanjala@Sammys-MacBook-Pro-2.local>
2022-12-07 15:47:01 +0100

    Extend button types

--- app/src/main/java/com/example/newsapp/composables/screens/screenmodels/ScreenType.kt
+++ app/src/main/java/com/example/newsapp/composables/screens/screenmodels/ScreenType.kt
@@ -2,5 +2,7 @@ package com.example.newsapp.composables.screens.screenmodels
 
 enum class ScreenType {
     NEWS_HOME,
-    NEWS_CATEGORY
+    NEWS_CATEGORY,
+    ONLINE_NEWS_SCREEN,
+    SAVED_NEWS_SCREEN
 }
