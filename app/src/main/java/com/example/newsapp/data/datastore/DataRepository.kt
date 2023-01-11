2cb379faff4d42673245b804620d5e43eb9c4615
sammywanjala <wanjala.sam.s@gmail.com>
2023-01-11 21:06:31 +0100

    Update: Update data source use and get all news logic

--- app/src/main/java/com/example/newsapp/data/datastore/DataRepository.kt
+++ app/src/main/java/com/example/newsapp/data/datastore/DataRepository.kt
@@ -3,14 +3,16 @@ package com.example.newsapp.data.datastore
 import android.util.Log
 import com.example.newsapp.data.model.Article
 import com.example.newsapp.data.model.News
+import com.example.newsapp.data.model.Sources
 import kotlinx.coroutines.flow.Flow
-
 import javax.inject.Inject
 
 interface DataRepository {
     suspend fun getSavedArticles(): Flow<List<Article>>
     suspend fun insertNewArticle(article: Article): Boolean
     suspend fun getNewsByCategory(category: String): News?
+    suspend fun getNewsFromDataSource(): Sources?
+    suspend fun getAllNews() : News?
 }
 
 class DataRepositoryImpl @Inject constructor(
@@ -19,6 +21,14 @@ class DataRepositoryImpl @Inject constructor(
 ) : DataRepository {
     override suspend fun getNewsByCategory(category: String): News? {
         return newsRemoteDataStore.getNewsByCategory(category)
+    }
+
+    override suspend fun getNewsFromDataSource(): Sources? {
+        return newsRemoteDataStore.getNewsFromSource()
+    }
+
+    override suspend fun getAllNews(): News? {
+        return newsRemoteDataStore.getAllNews()
     }
 
     override suspend fun getSavedArticles(): Flow<List<Article>> {
