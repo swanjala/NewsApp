ef8dd51ed402c538e4c1dfacc5ff76b9326e2f49
Sammy Wanjala <sammy.wamalwa@rikstv.no>
2023-12-01 04:32:39 +0100

    Update components and references

--- app/src/main/java/com/example/newsapp/composables/components/SourcesCard.kt
+++ app/src/main/java/com/example/newsapp/composables/components/SourcesCard.kt
@@ -3,7 +3,6 @@ package com.example.newsapp.composables.components
 import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.newsapp.composables.components.SourcesCardDimens.roundedCornerDimens
