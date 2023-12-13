60980a071ca5b8ed1ffb3a44c81f2df69e6fbbb8
swanjala <wanjala.sam.s@gmail.com>
2023-12-13 15:16:29 +0100

    Simplify view model options and query by category

--- app/src/main/java/com/example/newsapp/news/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/news/NewsViewModel.kt
@@ -1,21 +1,11 @@
 package com.example.newsapp.news
 
 import android.util.Log
-import androidx.compose.ui.text.intl.Locale
-import androidx.compose.ui.text.toLowerCase
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.newsapp.composables.composableModels.NewsUiState
 import com.example.newsapp.data.datastore.DataRepository
 import com.example.newsapp.data.model.Article
-import com.example.newsapp.data.model.NewsCategory
-import com.example.newsapp.data.model.NewsCategory.BUSINESS
-import com.example.newsapp.data.model.NewsCategory.ENTERTAINMENT
-import com.example.newsapp.data.model.NewsCategory.GENERAL
-import com.example.newsapp.data.model.NewsCategory.KEYWORD
-import com.example.newsapp.data.model.NewsCategory.SCIENCE
-import com.example.newsapp.data.model.NewsCategory.SPORTS
-import com.example.newsapp.data.model.NewsCategory.TECHNOLOGY
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -29,28 +19,26 @@ class NewsViewModel @Inject constructor(
     private var _newsUiState = MutableStateFlow(NewsUiState())
     val uiState: StateFlow<NewsUiState> = _newsUiState.asStateFlow()
 
-    fun updateData(requestCategory: NewsCategory) = viewModelScope.launch {
-        when (requestCategory) {
-            BUSINESS -> {
-                getNewsCategory(BUSINESS)
-            }
-            ENTERTAINMENT -> {
-                getNewsCategory(ENTERTAINMENT)
-            }
-            GENERAL -> {
-                getNewsCategory(GENERAL)
-            }
-            TECHNOLOGY -> {
-                getNewsCategory(TECHNOLOGY)
-            }
-            SCIENCE -> {
-                getNewsCategory(SCIENCE)
-            }
-            SPORTS -> {
-                getNewsCategory(SPORTS)
-            }
-            else -> {
-                getNewsCategory(KEYWORD)
+    fun getNewsByCategory(requestType: String) = viewModelScope.launch {
+
+        if (requestType.isEmpty()) {
+            getAllNews()
+        } else {
+            try {
+                val newsItems = dataRepository.getNewsByCategory(
+                    category = requestType
+                )
+                newsItems?.let {
+                    _newsUiState.value = NewsUiState(
+                        newsItems = it.articles,
+                        errorState = false
+                    )
+                }
+            } catch (error: Throwable) {
+                Log.getStackTraceString(error)
+                _newsUiState.value = NewsUiState(
+                    errorState = true
+                )
             }
         }
     }
@@ -89,27 +77,6 @@ class NewsViewModel @Inject constructor(
                     )
                 }
             }
-        }
-    }
-
-    private suspend fun getNewsCategory(category: NewsCategory) {
-        try {
-            val newsItems = dataRepository.getNewsByCategory(
-                category = category.toString().toLowerCase(
-                    Locale.current
-                )
-            )
-            newsItems?.let {
-                _newsUiState.value = NewsUiState(
-                    newsItems = it.articles,
-                    errorState = false
-                )
-            }
-        } catch (error: Throwable) {
-            Log.getStackTraceString(error)
-            _newsUiState.value = NewsUiState(
-                errorState = true
-            )
         }
     }
 
