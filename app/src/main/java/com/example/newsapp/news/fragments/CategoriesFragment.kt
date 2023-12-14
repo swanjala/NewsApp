1c66ec4883b73ec8b2e11f1822abc87003a160b6
swanjala <wanjala.sam.s@gmail.com>
2023-12-14 16:38:52 +0100

    Disable categories fragmen

--- app/src/main/java/com/example/newsapp/news/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/newsapp/news/fragments/CategoriesFragment.kt
@@ -5,20 +5,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
-import com.example.newsapp.composables.screens.SelectionScreenComposable
 import com.example.newsapp.composables.screens.screenmodels.HomeButtonItem
 import com.example.newsapp.composables.screens.screenmodels.ScreenType
 import com.example.newsapp.composables.screens.screenmodels.SourceType
+import com.example.newsapp.data.model.NewsCategory
+import com.example.newsapp.news.NewsViewModel
 import com.example.newsapp.news.extensions.composeView
-import com.example.newsapp.ui.theme.NewsAppTheme
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class CategoriesFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModel: NewsViewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[NewsViewModel::class.java]
+    }
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.run {
+            viewModelScope.launch {
+                getNewsByCategory(NewsCategory.EVERYTHING.type)
+            }
+        }
     }
 
     override fun onCreateView(
@@ -26,19 +46,22 @@ class CategoriesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = composeView {
-        NewsAppTheme {
+        /*
+                NewsAppTheme {
             SelectionScreenComposable(
                 screenType = ScreenType.NEWS_CATEGORY,
+                viewModel = viewModel,
                 handleHomeSelection = ::handleCategoriesNavigation
             )
         }
+       */
     }
 
     private fun handleCategoriesNavigation(item: HomeButtonItem, screenType: ScreenType) {
         val action = CategoriesFragmentDirections.nextAction(
-                item.newsSourceCategory ?: "news",
-                SourceType.ONLINE,
-            )
+            item.newsSourceCategory ?: "news",
+            SourceType.ONLINE,
+        )
 
         findNavController().navigate(action)
     }
